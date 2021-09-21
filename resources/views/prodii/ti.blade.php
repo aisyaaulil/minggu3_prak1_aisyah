b97426d73c5b8b2b0362565ebf7231b9b8e1d6eb
aisyaaulil <aisyahmaulina9@gmail.com>
2021-09-21 11:20:31 +0700

    Minggu 3 praktikum 1

---
+++ resources/views/prodii/ti.blade.php
@@ -0,0 +1,9 @@
+<html>
+    <head>
+        <title>Teknik Informatika</title>
+    </head>
+    <body>
+        <h1 align="center"><font face= "Courier New" color="Blue" >Program Studi Teknik Informatika</font1></h1>
+        <p> Ini merupakan Halaman Website untuk Program Studi D-IV Teknik Informatika Politeknik Negeri Malang</p>
+</body>
+</html>
